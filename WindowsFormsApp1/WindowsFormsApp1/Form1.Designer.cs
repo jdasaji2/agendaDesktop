69fedae3a7a9728635b46d13ac03880f03fe4dc6
jhoel daniel salinas jimenez <jsdaniel_1992@hotmail.com>
2018-04-15 18:32:36 -0500

    cvvc

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(412, 85);
+            this.button1.Location = new System.Drawing.Point(452, 165);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
